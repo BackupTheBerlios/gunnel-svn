dcb3eb8754c40a14e92ea4ed5b9bd9156281fbc2
meand <meand@025d7191-19dd-46a1-b737-2e6d51d81149>
2010-07-22 14:16:47 +0000

    Grovt ombyggd "plain-to-plain" för full bakgrundskörning.

--- trunk/plain-to-plain.c
+++ trunk/plain-to-plain.c
@@ -29,13 +29,20 @@
 #include <grp.h>
 #include <pwd.h>
 
-static const char options_string[] = "hl:r:g:u:";
+static const char options_string[] = "hl:r:g:u:o";
 
 /* Semaphores for flow control. */
 static int show_usage = 0;
+static int again = 1;
 
-/* The tunnel digger and transporter. */
-int establish_tunnel(int sd, char *rhost, char *rport);
+/* Drop privileges, become daemon. */
+int underpriv_daemon_mode(void);
+
+/* Traffic exchanger. */
+static void transmitter(int td, int rd);
+
+/* Looping for incoming clients. */
+static int accept_loop(int sd, char *rhost, char *rport);
 
 /* Return "none" if argument is null. */
 static inline const char *cover_empty_string(const char *str) {
@@ -48,6 +55,7 @@ static void show_info(char *progname) {
 						REMOTE_PORT_STR
 						TUNNEL_USR_STR
 						TUNNEL_GRP_STR
+						ONE_SHOT_STR
 						"\n\n",
 				progname);
 
@@ -55,14 +63,33 @@ static void show_info(char *progname) {
 			"\tProcess owner:   %s\n"
 			"\tProcess group:   %s\n"
 			"\tLocal port:      %s\n"
-			"\tRemote port:     %s\n",
+			"\tRemote port:     %s\n"
+			"\tOne shot server: %s\n",
 			cover_empty_string(user_name),
 			cover_empty_string(group_name),
 			cover_empty_string(local_port_string),
-			cover_empty_string(remote_port_string)
+			cover_empty_string(remote_port_string),
+			again ? "false" : "true"
 			);
 	exit(EXIT_FAILURE);
 }; /* show_info(char *) */
+
+/**
+ * signal_responder  --  respond to select signals
+ */
+
+static void signal_responder(int sig) {
+	switch (sig) {
+		case SIGTERM:
+			again = 0;
+			break;
+		case SIGCHLD:
+			while ( waitpid(-1, NULL, WNOHANG) > 0 )
+				;
+		default:
+			break;
+	}
+}; /* signal_responder(int) */
 
 /*
  * Main control for this subsystem.
@@ -88,6 +115,9 @@ int plain_to_plain(int argc, char *argv[]) {
 						break;
 			case TUNNEL_GRP:
 						group_name = optarg;
+						break;
+			case ONE_SHOT:
+						again = 0;
 						break;
 			case '?':
 			default:
@@ -164,147 +194,30 @@ int plain_to_plain(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	if ( (rc = establish_tunnel(sd, rhost, rport)) )
-		gunnel_error_message(stderr, rc);
-	else
-		fprintf(stderr, "Successful service.\n");
+	if ( (rc = underpriv_daemon_mode()) != GUNNEL_SUCCESS )
+		return rc;
+
+	/* Put the listener to work. */
+	rc = accept_loop(sd, rhost, rport);
 
 	close(sd);
 
-	return EXIT_SUCCESS;
+	return rc;
 }; /* plain_to_plain(int, char *[]) */
 
 /**
- * establish_tunnel  --  create and service the tunnel
+ * transmitter  --  send data to and fro
  */
-int establish_tunnel(int sd, char *rhost, char *rport) {
-	int ret, td = -1, rd = -1, maxfd;
+
+static void transmitter(int td, int rd) {
+	ssize_t n;
+	int maxfd;
 	fd_set fdset;
-	socklen_t socklen;
-	struct sockaddr_storage addr;
-	struct addrinfo hints, *ai, *aiptr;
-	pid_t pid;
-	struct passwd *passwd;
-	struct group *group;
-
-	socklen = sizeof(addr);
-	if ( (td = accept(sd, (struct sockaddr *) &addr, &socklen)) < 0 ) {
-		//perror("Tunnel");
-		return EXIT_FAILURE;
-	}
-
-	/* Forking and intending an underprivileged
-	 * process owner. */
-	switch (pid = fork()) {
-		case -1:
-			/* Failure */
-			return GUNNEL_FORKING;
-			break;
-		case 0:
-			/* Child process continues the work. */
-			break;
-		default:
-			/* Parent process only waits for status. */
-			waitpid(pid, &ret, 0);
-			return ret;
-			break;
-	}
-
-	if (setsid() < 0)
-		return GUNNEL_FORKING;
-
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	close(sd);		/* The listening socket. */
-
-	signal(SIGHUP, SIG_IGN);
-	signal(SIGINT, SIG_IGN);
-	signal(SIGTSTP, SIG_IGN);
-
-	/* Change UID/GID in child process.
-	 * The feasibility has previously been
-	 * asserted. */
-	group = getgrnam(group_name);
-	passwd = getpwnam(user_name);
-
-	/* Must begin with setgid! */
-	if ( setgid(group->gr_gid) < 0 )
-		exit(GUNNEL_FAILED_GID);
-
-	/* Commence with UID. */
-	if ( setuid(passwd->pw_uid) < 0 )
-		exit(GUNNEL_FAILED_UID);
-
-	memset(&hints, '\0', sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-#if defined(__linux__) || defined(__FreeBSD__)
-	hints.ai_flags = AI_ADDRCONFIG;
-#endif
-
-	if ( (ret = getaddrinfo(rhost, rport, &hints, &aiptr)) ) {
-		shutdown(td, SHUT_RDWR);
-		close(td);
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret));
-		return GUNNEL_FAILED_REMOTE_CONN;
-	}
-
-	for (ai = aiptr; ai; ai = ai->ai_next) {
-		if ( (rd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol)) < 0 )
-			continue;
-
-		if ( connect(rd, ai->ai_addr, ai->ai_addrlen) < 0 ) {
-			close(rd);
-			rd = -1;
-			continue;
-		}
-
-		/* Successfully connected. */
-		break;
-	}
-
-	freeaddrinfo(aiptr);
-
-	if (ai == NULL) {
-		/* Failure when locating the remote host. */
-		shutdown(td, SHUT_RDWR);
-		close(td);
-		return GUNNEL_FAILED_REMOTE_CONN;
-	}
-
-	/* At this stage the connection is working,
-	 * only traffic remains. Time to finally
-	 * fork off the working daemon. */
-
-	close(STDERR_FILENO);
-
-	switch (pid = fork()) {
-		case -1:
-			/* Failure to fork. Close everything down. */
-			shutdown(rd, SHUT_RDWR);
-			shutdown(td, SHUT_RDWR);
-			close(rd);
-			close(td);
-			close(sd);
-			exit(GUNNEL_FORKING);
-			break;
-		case 0:
-			/* Working offspring. */
-			break;
-		default:
-			/* This parent reports success. */
-			exit(GUNNEL_SUCCESS);
-			break;
-	}
-
-	/* Move somewhere relatively safe. */
-	chdir(FORKDIR);
+	char recvbuf[2048];
 
 	maxfd = (rd > td) ? rd : td;
 
 	while(1) {
-		ssize_t n;
-		char recvbuf[2048];
 
 		FD_ZERO(&fdset);
 		FD_SET(rd, &fdset);
@@ -364,6 +277,163 @@ int establish_tunnel(int sd, char *rhost, char *rport) {
 	shutdown(td, SHUT_RDWR);
 	close(rd);
 	close(td);
+}; /* transmitter(int, int) */
+
+static int accept_loop(int sd, char *rhost, char *rport) {
+	int ret, td = -1, rd = -1;
+	pid_t pid;
+	socklen_t socklen;
+	struct sockaddr_storage addr;
+	struct addrinfo hints, *ai, *aiptr;
+
+	do {
+		socklen = sizeof(addr);
+
+		if ( (td = accept(sd, (struct sockaddr *) &addr, &socklen)) < 0 ) {
+			if (! again)
+				break;
+			if (errno == EINTR)
+				continue;
+			else
+				continue;
+		}
+
+		memset(&hints, '\0', sizeof(hints));
+		hints.ai_family = AF_UNSPEC;
+		hints.ai_socktype = SOCK_STREAM;
+#if defined(__linux__) || defined(__FreeBSD__)
+		hints.ai_flags = AI_ADDRCONFIG;
+#endif
+
+		if ( (ret = getaddrinfo(rhost, rport, &hints, &aiptr)) ) {
+			shutdown(td, SHUT_RDWR);
+			close(td);
+			return GUNNEL_FAILED_REMOTE_CONN;
+		}
+
+		for (ai = aiptr; ai; ai = ai->ai_next) {
+			if ( (rd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol)) < 0 )
+				continue;
+
+			if ( connect(rd, ai->ai_addr, ai->ai_addrlen) < 0 ) {
+				close(rd);
+				rd = -1;
+				continue;
+			}
+
+			/* Successfully connected. */
+			break;
+		}
+
+		freeaddrinfo(aiptr);
+
+		if (ai == NULL) {
+			/* Failure when locating the remote host. */
+			shutdown(td, SHUT_RDWR);
+			close(td);
+			return GUNNEL_FAILED_REMOTE_CONN;
+		}
+
+		/* At this stage the connection is working,
+		 * only traffic remains. Time to finally
+		 * fork off the working daemon. */
+
+		switch (pid = fork()) {
+			case -1:
+				/* Failure to fork. Close everything down. */
+				shutdown(rd, SHUT_RDWR);
+				shutdown(td, SHUT_RDWR);
+				close(rd);
+				close(td);
+				close(sd);
+				exit(GUNNEL_FORKING);
+			case 0:
+				/* Working offspring. */
+				/* The listening socket is no longer needed. */
+				close(sd);
+				/* Move somewhere relatively safe. */
+				transmitter(td, rd);
+				exit(GUNNEL_SUCCESS);
+			default:
+				/* This parent reports success. */
+				close(td);
+				close(rd);
+				break;
+		}
+
+	} while (again);
 
 	exit(GUNNEL_SUCCESS);
-}; /* establish_tunnel(int, char *, char *) */
+}; /* accept_loop(int, char *, char *) */
+
+/**
+ * Change GID/UID and enter daemon mode.
+ */
+int underpriv_daemon_mode(void) {
+	pid_t pid;
+	struct passwd *passwd;
+	struct group *group;
+
+	signal(SIGTERM, signal_responder);
+	signal(SIGCHLD, signal_responder);
+
+	/* Forking and intending an underprivileged
+	 * process owner. */
+	switch (pid = fork()) {
+		case -1:
+			/* Failure */
+			return GUNNEL_FORKING;
+			break;
+		case 0:
+			/* Child process continues the work. */
+			break;
+		default:
+			/* Parent process only exits. */
+			exit(GUNNEL_SUCCESS);
+			break;
+	}
+
+	if (setsid() < 0)
+		return GUNNEL_FORKING;
+
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+
+	signal(SIGHUP, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
+
+	chdir(FORKDIR);
+
+	/* Change UID/GID in child process.
+	 * The feasibility has previously been
+	 * asserted. */
+	group = getgrnam(group_name);
+	passwd = getpwnam(user_name);
+
+	/* Must begin with setgid! */
+	if ( setgid(group->gr_gid) < 0 )
+		return GUNNEL_FAILED_GID;
+
+	/* Commence with UID. */
+	if ( setuid(passwd->pw_uid) < 0 )
+		return GUNNEL_FAILED_UID;
+
+	switch (pid = fork()) {
+		case -1:
+			return GUNNEL_FORKING;
+			break;
+		case 0:
+			/* Worker child process. */
+			break;
+		default:
+			/* Intermediary parent is expendable. */
+			exit(GUNNEL_SUCCESS);
+			break;
+	}
+
+	/* Now all error messages are superfluous. */
+	close(STDERR_FILENO);
+
+	return GUNNEL_SUCCESS;
+}; /* underpriv_daemon_mode(void) */
